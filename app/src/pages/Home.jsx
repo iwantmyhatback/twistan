dd566995c78b850217c9d882b3944ec5e1eef235
iwantmyhatback <iwantmyhatbackk@gmail.com>
2024-10-08 22:17:58 -0400

    static button

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,71 +1,78 @@
 import { useState } from 'react';
 
 function Home() {
-	const images = [
-		"https://c.tenor.com/Qy5sUxL5phgAAAAC/tenor.gif",
-		"https://i.giphy.com/3bc9YL28QWi3pYzi1p.webp",
-		"https://i.giphy.com/WpIPS0DWNpMm4kfMVr.webp",
-		"https://i.giphy.com/Xev2JdopBxGj1LuGvt.webp",
-		"https://i.giphy.com/IThjAlJnD9WNO.webp",
-		"https://i.giphy.com/ASd0Ukj0y3qMM.webp",
-		"https://i.giphy.com/PK1YQhAoBOpP2.webp",
-		"https://i.giphy.com/8vc2rMUDjhy6Y.webp",
-		"https://i.giphy.com/W3keANaGsQLC5Ri8DM.webp",
-		"https://i.giphy.com/13TXV4kfn7r2iA.webp",
-		"https://i.giphy.com/8LDHJbotKQyp99YZCQ.webp",
-		"https://i.giphy.com/8JQOSCa7aJSbtVke4X.webp",
-		"https://i.giphy.com/JRgtgsZQSNYA5wEA6d.webp",
-		"https://c.tenor.com/59_-QOhxOcIAAAAC/tenor.gif",
-		"https://media.tenor.com/Feqwt9mkzq0AAAAM/2pac-waves.gif",
-		"https://media.tenor.com/neqnFd4CHWAAAAAM/up-wave.gif"
-	];
+  const images = [
+	"https://c.tenor.com/Qy5sUxL5phgAAAAC/tenor.gif",
+	"https://i.giphy.com/3bc9YL28QWi3pYzi1p.webp",
+	"https://i.giphy.com/WpIPS0DWNpMm4kfMVr.webp",
+	"https://i.giphy.com/Xev2JdopBxGj1LuGvt.webp",
+	"https://i.giphy.com/IThjAlJnD9WNO.webp",
+	"https://i.giphy.com/ASd0Ukj0y3qMM.webp",
+	"https://i.giphy.com/PK1YQhAoBOpP2.webp",
+	"https://i.giphy.com/8vc2rMUDjhy6Y.webp",
+	"https://i.giphy.com/W3keANaGsQLC5Ri8DM.webp",
+	"https://i.giphy.com/13TXV4kfn7r2iA.webp",
+	"https://i.giphy.com/8LDHJbotKQyp99YZCQ.webp",
+	"https://i.giphy.com/8JQOSCa7aJSbtVke4X.webp",
+	"https://i.giphy.com/JRgtgsZQSNYA5wEA6d.webp",
+	"https://c.tenor.com/59_-QOhxOcIAAAAC/tenor.gif",
+	"https://media.tenor.com/Feqwt9mkzq0AAAAM/2pac-waves.gif",
+	"https://media.tenor.com/neqnFd4CHWAAAAAM/up-wave.gif"
+  ];
 
-	const [imgUrl, setImgUrl] = useState(images[0]);
-	const [fade, setFade] = useState(false);
+  const [imgUrl, setImgUrl] = useState(images[0]);
+  const [fade, setFade] = useState(false);
 
-	const handleNewImage = () => {
-		setFade(true);
-		setTimeout(() => {
-			let newImg = getRandomElementSecure(images)
-			while(imgUrl == newImg){
-				newImg = getRandomElementSecure(images)
-			}
-			setImgUrl(newImg);
-			setFade(false); 
-		}, 300); 
-	};
+  
+  const handleNewImage = () => {
+	setFade(true);
+	setTimeout(() => {
+	let newImg = getRandomElementSecure(images);
+		
+	while (imgUrl === newImg) {
+		newImg = getRandomElementSecure(images);
+	}
+	setImgUrl(newImg);
+	setFade(false); 
+	}, 300); 
+  };
 
-	return (
-		<div className="">
-			<div className="flex-col translate-y-[-10vh]">
-				<div className="header-base flex justify-center">
-					[ Twistan says hello ]
-				</div>
-				<div className='flex justify-center'>
-					<img
-						src={imgUrl}
-						alt="Wave"
-						className={`transition-opacity duration-300 ${fade ? 'opacity-0' : 'opacity-100'}`} 
-					/>
-				</div>
-			</div>
-			<div className="flex justify-center mt-4">
-				<button
-					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-					onClick={handleNewImage}
-				>
-					Wave Back!
-				</button>
+  return (
+	<div className="flex flex-col items-center justify-center h-screen">
+		<div className="text-center mb-4">
+			<div className="header-base">
+			[] Twistan says hello ]
 			</div>
 		</div>
-	);
+		
+		{/* Image Container */}
+		<div className="w-[300px] h-[300px] flex items-center justify-center mb-4">
+			<img
+			src={imgUrl}
+			alt="Wave"
+			className={`transition-opacity duration-300 object-contain max-h-full max-w-full ${fade ? 'opacity-0' : 'opacity-100'}`} 
+			/>
+		</div>
+
+		{/* Button */}
+		<div className="fixed bottom-10">
+			<button
+			className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+			onClick={handleNewImage}
+			>
+			Wave Back!
+			</button>
+		</div>
+	</div>
+  );
 }
 
+
 function getRandomElementSecure(arr) {
-	const randomArray = new Uint32Array(1);
-	window.crypto.getRandomValues(randomArray);
-	const randomIndex = randomArray[0] % arr.length;
-	return arr[randomIndex];
+  const randomArray = new Uint32Array(1);
+  window.crypto.getRandomValues(randomArray);
+  const randomIndex = randomArray[0] % arr.length;
+  return arr[randomIndex];
 }
 
 export default Home;
