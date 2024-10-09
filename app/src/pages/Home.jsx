3e303966e29df28d2dd778862624064973e2da12
iwantmyhatback <iwantmyhatbackk@gmail.com>
2024-10-08 20:36:32 -0400

    add a button

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,59 +1,56 @@
 import { useState } from 'react';
 
-
 function Home() {
-	const [visits, setVisits] = useState('');
-	const images = [
-		"https://c.tenor.com/Qy5sUxL5phgAAAAC/tenor.gif",
-		"https://i.giphy.com/3bc9YL28QWi3pYzi1p.webp",
-		"https://i.giphy.com/WpIPS0DWNpMm4kfMVr.webp",
-		"https://i.giphy.com/Xev2JdopBxGj1LuGvt.webp",
-		"https://i.giphy.com/IThjAlJnD9WNO.webp",
-		"https://i.giphy.com/ASd0Ukj0y3qMM.webp",
-		"https://i.giphy.com/PK1YQhAoBOpP2.webp",
-		"https://i.giphy.com/8vc2rMUDjhy6Y.webp",
-		"https://i.giphy.com/W3keANaGsQLC5Ri8DM.webp",
-		"https://i.giphy.com/13TXV4kfn7r2iA.webp",
-		"https://i.giphy.com/8LDHJbotKQyp99YZCQ.webp",
-		"https://i.giphy.com/8JQOSCa7aJSbtVke4X.webp",
-		"https://i.giphy.com/JRgtgsZQSNYA5wEA6d.webp",
-		"https://c.tenor.com/59_-QOhxOcIAAAAC/tenor.gif",
-		"https://media.tenor.com/Feqwt9mkzq0AAAAM/2pac-waves.gif",
-		"https://media.tenor.com/neqnFd4CHWAAAAAM/up-wave.gif"
+  const images = [
+    "https://c.tenor.com/Qy5sUxL5phgAAAAC/tenor.gif",
+    "https://i.giphy.com/3bc9YL28QWi3pYzi1p.webp",
+    "https://i.giphy.com/WpIPS0DWNpMm4kfMVr.webp",
+    "https://i.giphy.com/Xev2JdopBxGj1LuGvt.webp",
+    "https://i.giphy.com/IThjAlJnD9WNO.webp",
+    "https://i.giphy.com/ASd0Ukj0y3qMM.webp",
+    "https://i.giphy.com/PK1YQhAoBOpP2.webp",
+    "https://i.giphy.com/8vc2rMUDjhy6Y.webp",
+    "https://i.giphy.com/W3keANaGsQLC5Ri8DM.webp",
+    "https://i.giphy.com/13TXV4kfn7r2iA.webp",
+    "https://i.giphy.com/8LDHJbotKQyp99YZCQ.webp",
+    "https://i.giphy.com/8JQOSCa7aJSbtVke4X.webp",
+    "https://i.giphy.com/JRgtgsZQSNYA5wEA6d.webp",
+    "https://c.tenor.com/59_-QOhxOcIAAAAC/tenor.gif",
+    "https://media.tenor.com/Feqwt9mkzq0AAAAM/2pac-waves.gif",
+    "https://media.tenor.com/neqnFd4CHWAAAAAM/up-wave.gif"
+  ];
 
+  const [imgUrl, setImgUrl] = useState(images[0]);
 
-	]
-
-	const imageUrl = "https://i.giphy.com/3bc9YL28QWi3pYzi1p.webp";
-	return (
-		<>
-			<div className="">
-				<div className="flex-col translate-y-[-10vh]">
-					<div className="header-base flex justify-center">
-						[ Twistan says hello ]
-					</div>
-					<div className=" flex justify-center">
-						<img src={getRandomElementSecure(shuffleArray(images))} alt="Wave"></img>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  const handleNewImage = () => {
+	setImgUrl(getRandomElementSecure(images));
+  };
+  
+  return (
+	<div className="">
+      <div className="flex-col translate-y-[-10vh]">
+        <div className="header-base flex justify-center">
+          [ Twistan says hello ]
+        </div>
+        <div className="flex justify-center">
+          <img src={imgUrl} alt="Wave" />
+        </div>
+        <div className="flex justify-center mt-4">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleNewImage}>
+            Wave Back
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
+
 
 function getRandomElementSecure(arr) {
-	const randomArray = new Uint32Array(1);
-	window.crypto.getRandomValues(randomArray);
-	const randomIndex = randomArray[0] % arr.length;
-	return arr[randomIndex];
+  const randomArray = new Uint32Array(1);
+  window.crypto.getRandomValues(randomArray);
+  const randomIndex = randomArray[0] % arr.length;
+  return arr[randomIndex];
 }
 
-function shuffleArray(array) {
-	for (let i = array.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1));
-		[array[i], array[j]] = [array[j], array[i]];
-	}
-	return array;
-}
 export default Home;
-
